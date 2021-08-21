aba104cb248c5bf36b23b709aace5c788c22f7f8
Fredi <Fredi1574@gmail.com>
2021-08-22 02:40:43 +0300

    v1

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const grid = document.querySelectorAll(".tile");
 let xTurn = true;
 
-let tile = {
-  marked: false,
-  innerHTML: "",
-};
+grid.forEach((tile) => {
+  /*
+  tile.addEventListener("mouseover", () => {
+    if (tile.innerHTML === "") {
+      if (xTurn) {
+        tile.innerHTML = "<i class='fas fa-times'>";
+      } else {
+        tile.innerHTML = "<i class='far fa-circle'>";
+      }
+      tile.style.color = "rgba(255,255,255,0.6)";
+    }
+  });
 
-grid.forEach((elem) => {
-  elem = new tile();
-  elem.addEventListener("click", () => {
-    if (elem.innerHTML !== "") {
+  tile.addEventListener("mouseout", () => {
+    tile.innerHTML = "";
+  }); */
+
+  tile.addEventListener("click", () => {
+    if (tile.innerHTML !== "") {
       return;
     }
-    xTurn === true
-      ? ((elem.innerHTML = "<i class='fas fa-times'>"), (elem.mark = "X"))
-      : ((elem.innerHTML = "<i class='far fa-circle'>"), (elem.mark = "O"));
+    xTurn
+      ? ((tile.innerHTML = "<i class='fas fa-times'>"), (tile.mark = "X"))
+      : ((tile.innerHTML = "<i class='far fa-circle'>"), (tile.mark = "O"));
     xTurn = !xTurn;
   });
 });
 
-function checkIfWin() {
-  for (let i = 0; i < grid.length; i++) {}
-}
+// function checkIfWin() {
+//   for (let i = 0; i < grid.length; i++) {}
+// }
+
+document.querySelector("#clearBoard").addEventListener("click", () => {
+  grid.forEach((tile) => {
+    tile.textContent = "";
+  });
+});
